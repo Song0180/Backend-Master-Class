9ac8dc30b81e10395f03bfb7ca29ef5d6aa74ff9
song0180 <song0180@e.ntu.edu.sg>
2024-01-22 23:18:36 +0800

    fix bi-diraction transfer deadlock

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -117,3 +117,59 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 }
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testConnPool)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+	// run n concurrent transactions
+	n := 10
+	amount := int64(10)
+
+	// use channels to communicate & share data with each concurrent transaction routine
+	// 1 for err, 1 for result
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountId := account2.ID
+
+		if i%2 == 0 {
+			fromAccountID = account2.ID
+			toAccountId = account1.ID
+		}
+
+		go func() {
+			ctx := context.Background()
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				Amount:        amount,
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountId,
+			})
+
+			errs <- err
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+	}
+
+	// check the upated balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
